e6ab28b1446d57037a4a4674f38f8619f321b15a
DANIEL MARTIN <1900930@uad.ac.uk>
2020-02-01 11:18:44 +0000

    Main Menu

---
+++ Re-Pair/Assets/Scripts/d_scripts/d_MainMenu/d_Buttons.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class d_Buttons : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject optionsMenu;
+
+    public void loadLevel(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void activateMenu()
+    {
+        mainMenu.SetActive(!mainMenu.activeSelf);
+        optionsMenu.SetActive(!optionsMenu.activeSelf);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
