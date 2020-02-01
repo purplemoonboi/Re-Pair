a7475d50a02f484355a1dedd3aed3f8a02e43b1e
DANIEL MARTIN <1900930@uad.ac.uk>
2020-02-01 20:18:21 +0000

    Added rotation to collectable spikey thingie

---
+++ Re-Pair/Assets/Scripts/d_scripts/d_Objects/d_Collectable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class d_Collectable : MonoBehaviour
+{
+    [Range(0, 100)] public float m_rotationSlider;
+
+    void Update()
+    {
+        rotateObject(m_rotationSlider);
+    }
+
+    private void rotateObject(float value)
+    {
+        this.gameObject.transform.Rotate(0, value * Time.deltaTime, 0);
+    }
+}
