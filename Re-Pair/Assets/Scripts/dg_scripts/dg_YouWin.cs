85e5898fe32a25700edb7140cbf48273789c076a
1900931 <davedouggreen@gmail.com>
2020-02-01 16:44:00 +0000

    Create game win UI
    Create game win UI to face in upon player triggering the last PORTAL.

---
+++ Re-Pair/Assets/Scripts/dg_scripts/dg_YouWin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class dg_YouWin : MonoBehaviour
+{
+    public TextMeshProUGUI m_congratulations;
+    float m_fadetime = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_congratulations.canvasRenderer.SetAlpha(0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FadeInText()
+    {
+        print("Fade called");
+        m_congratulations.CrossFadeAlpha(1.0f, m_fadetime, false);
+    }
+}
