21d4ba33cc8b30b2b0ac21ff1d8562a47b13f9b6
ythen1011 <ythen1995@hotmail.co.uk>
2020-02-02 13:03:41 +0000

    push rotation

---
+++ Re-Pair/Assets/ArtTeams/Prefabs/ColourShift.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourShift : MonoBehaviour
+{
+ 
+    void Start()
+    {
+     
+        //Material mymat = GetComponent<Renderer>().material;
+        //    mymat.SetColor("_EmissionColor", Color.red);
+        StartCoroutine("Update");
+    }
+
+    // Update is called once per frame
+    IEnumerable Update()
+    {
+        Material mymat = GetComponent<Renderer>().material;
+        mymat.SetColor("_EmissionColor", Color.HSVToRGB(1f, 1f, 1f));
+        yield return new WaitForSeconds(5);
+
+    }
+}
